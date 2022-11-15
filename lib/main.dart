6cd5df2322382e9458cdcda5bef37af53f115331
Cristian Alfredo Manzo Parra <ca.manzo973@uniandes.edu.co>
2022-11-14 20:07:53 -0500

    Init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // runApp(new MyApp())
+  runApp(const MyApp());
+}
+
+// Stateless no mantienen el estado de la aplicacion
+// no mantienen los valores por si mismo
+// ? todo metodo StatelessWidged tiene un metodo Build
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Text('Hola Mundo'),
+      ),
+    );
+  }
+}
